b8679a4f676d6c7e3a5616e5f70d7eb30a99cbc7
Cj Buresch <cjburesch@gmail.com>
2014-04-14 21:57:18 -0700

    preparing to merge back to master
    added about, help and finishing test

--- src/lambdaCrypto/Test.java
+++ src/lambdaCrypto/Test.java
@@ -11,26 +11,29 @@ public class Test {
 	public static void main(String[] args) {
 		Test tester = new Test();
 		// Get Run-Mode
-		for (int i = 0; i < args.length; i++) {
+		if (args.length > 0) {
 			// HELP CMD
-			if (args[i].equals("-help")) {
-				tester.printHelp();
+			if (args[0].equals("-help")) {
+				tester.printOut("/testpack/help.txt");
 				System.exit(0);
-			} else if (args[i].equals("-e")) { // ENCRYPT CMD
-				tester.setMode(RunMode.ENCRYPT);
-				tester.Run(args);
+			} else if (args[0].equals("-about")) {
+				tester.printOut("/testpack/about.txt");
 				System.exit(0);
-			} else if (args[i].equals("-d")) { // DECRYPT CMD
+			} else if (args[0].equals("-d")) { // DECRYPT CMD
 				tester.setMode(RunMode.DECRYPT);
 				tester.Run(args);
 				System.exit(0);
+			} else if (args[0].equals("-e")) { // ENCRYPT CMD
+				tester.setMode(RunMode.ENCRYPT);
+				tester.Run(args);
+				System.exit(0);
 			} else {
-				System.err.println("invalid argument:" + args[i]);
+				System.err.println("invalid argument:" + args[0]);
 				System.exit(1);
 			}
 		}
 		// RUNDEFAULT
-
+		tester.Run(null);
 	}
 
 	public enum RunMode {
@@ -56,7 +59,11 @@ public class Test {
 	private String infile = "src/testpack/in.txt";
 	private String firstoutfile = "src/testpack/first_out.txt";
 	private String finaloutfile = "src/testpack/final_out.txt";
-	private String cipher = "default cipher"; //TODO
+	private String cipher = "default cipher";
+	// TODO
+	private String blockmode = "default blockmode";
+
+	// TODO
 
 	public Test() {
 		runmode = RunMode.DEFAULT;
@@ -66,34 +73,50 @@ public class Test {
 		runmode = mode;
 		return false;
 	}
-	
+
 	private void Run(String[] args) {
-		setFiles(args);
-		if(runmode == RunMode.DEFAULT){
-
-		}else if (runmode == RunMode.ENCRYPT){
-
-		}else if (runmode == RunMode.ENCRYPT){
+		if (args != null) {
+			setFiles(args);
+			if (runmode == RunMode.ENCRYPT) {
+				// TODO
+			} else if (runmode == RunMode.ENCRYPT) {
+				// TODO
+			}
+		} else {
 
 		}
 	}
-	
-	private void setFiles(String[] args){
-		if(args.length == 4){
-			cipher = args[1];
-			infile = args[2];
-			firstoutfile = args[3]; 
-			finaloutfile = args[3] + "_final";
-		}else if (args.length == 3){
-			cipher = args[1];
-			infile = args[2];
-		}else if (args.length == 2){
-			cipher = args[1];
+
+	private void setFiles(String[] args) {
+		if (args[1] != "-x") {
+			if (args.length == 5) {
+				cipher = args[1];
+				blockmode = args[2];
+				infile = args[3];
+				firstoutfile = args[4];
+				finaloutfile = args[4] + "_final";
+			} else if (args.length == 4) {
+				cipher = args[1];
+				blockmode = args[2];
+				infile = args[3];
+			} else if (args.length == 3) {
+				cipher = args[1];
+				blockmode = args[2];
+			}else if (args.length == 2) {
+				cipher = args[1];
+			}
+		} else {
+			if (args.length == 4) {
+				infile = args[2];
+				firstoutfile = args[3] + ".txt";
+				finaloutfile = args[3] + "_final.txt";
+			} else if (args.length == 3) {
+				infile = args[2];
+			}
 		}
 	}
 
-	private void printHelp() {
-		String filename = "/testpack/help.txt";
+	private void printOut(String filename) {
 		File file = new File(filename);
 		try {
 			Scanner in = new Scanner(file);
@@ -104,9 +127,8 @@ public class Test {
 			}
 			in.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("Failure to open help.txt");
+			System.out.println("Failure to open file to be displayed");
 			e.printStackTrace();
 		}
 	}
-
 }
