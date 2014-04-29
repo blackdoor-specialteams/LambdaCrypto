895c311a87bfc928768c5fdab0f22d1a22918435
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-04-28 21:12:07 -0700

    Revert "Update Modes.java"
    This reverts commit 781bc07f4f95f8cd20b0d1dc5738817323958c35.

--- src/lambdaCrypto/Modes.java
+++ src/lambdaCrypto/Modes.java
@@ -14,10 +14,7 @@ public class Modes {
 	 * @param IV
 	 * @return a 2-tuple in the form (outputText, next) where next is the IV for the next block.
 	 */
-	 ////////////////////////////////////////////////////////////////////////
-	 ///		OUTPUT FEEDBACK			/////////////////////////
-	 ////////////////////////////////////////////////////////////////////////
-	public static TwoTuple<byte[], byte[]> OFBEncrypt(CipherAlgorithm algo, byte[] key, byte[] inputText, byte[] IV){
+	public static TwoTuple<byte[], byte[]> OFB(CipherAlgorithm algo, byte[] key, byte[] inputText, byte[] IV){
 		byte[] next;
 		byte[] outputText;
 		next = algo.cryptBlock(key, IV);
@@ -25,74 +22,15 @@ public class Modes {
 		return new TwoTuple<byte[], byte[]>(outputText, next);
 	}
 	
-	public static TwoTuple<byte[], byte[]> OFBDecrypt(CipherAlgorithm algo, byte[] key, byte[] inputText, byte[] IV){
-		byte[] next;
-		byte[] outputText;
-		
-		next = algo.cryptBlock(key, IV);
-		outputText = Misc.XOR(next, inputText);
-		return new TwoTuple<byte[], byte[]>(outputText, next);
-	}	
 	public static BlockCipherMode getOFB(){
 		return (CipherAlgorithm algo, byte[] key, byte[] inputText, byte[] IV) -> OFB(algo, key, inputText, IV);
 	}
 	
-	 ////////////////////////////////////////////////////////////////////////
-	 ///		ELECTRONIC CODEBOOK		/////////////////////////
-	 ////////////////////////////////////////////////////////////////////////	
-	public static TwoTuple<byte[], byte[]> ECBEncrypt(CipherAlgoritm algo, byte[] key, byte[] inputText, byte[] IV){
+	public static TwoTuple<byte[], byte[]> ECB(CipherAlgoritm algo, byte[] key, byte[] inputText, byte[] IV){
 		byte[] cipherText;
 		cipherText = algo.cryptBlock(key, inputText);
 		return new TwoTuple<byte[], Void>(outputText, null);
 	}
-	public static TwoTuple<byte[], byte[]> ECBDecrypt(CipherAlgoritm algo, byte[] key, byte[] inputText, byte[] IV){
-		byte[] plainText;
-		plainText = algo.cryptBlock(key, inputText);
-		return new TwoTuple<byte[], Void>(outputText, null);
-	}	
+
 	
-	public static BlockCipherMode getECB(){
-		return (CipherAlgorithm algo, byte[] key, byte[] inputText, byte[] IV) -> EBC(algo, key, inputText, IV);	
-
-	 ////////////////////////////////////////////////////////////////////////
-	 ///		CIPHER BLOCK CHAINING		/////////////////////////
-	 ////////////////////////////////////////////////////////////////////////
-
-	public static TwoTuple<byte[], byte[]> CBCEncrypt(CipherAlgoritm algo, byte[] key, byte[] inputText, byte[] IV){
-		byte[] next;
-		byte[] outputText;
-		byte[] bceText;
-		bceText = Misc.XOR(inputText, IV);
-		
-		next = algo.cryptBlock(key, bceText);
-		
-		outputText = next;
-		return new TwoTuple<byte[], byte[]>(outputText, next)
-	}
-	public static TwoTuple<byte[], byte[]> CBCDecrypt(CipherAlgoritm algo, byte[] key, byte[] inputText, byte[] IV){
-		byte[] next;
-		byte[] outputText;
-		byte[] bceText;
-		next = inputText;
-		
-		bceText = algo.cryptBlock(key, inputText)
-		outputText = Misc.XOR(bceText, IV)
-		
-		return new TwoTuple<byte[], byte[]>(outputText, next)
-	}
-		
-	public static BlockCipherMode getCBC(){
-		return (CipherAlgorithm algo, byte[] key, byte[] inputText, byte[] IV) -> CBC(algo, key, inputText, IV);	
-/*	 
-	 ////////////////////////////////////////////////////////////////////////
-	 ///		CIPHER FEEDBACK			/////////////////////////
-	 ////////////////////////////////////////////////////////////////////////
-	 
-	public static TwoTuple<byte[], byte[]> CFB(CipherAlgoritm algo, byte[] key, byte[] inputText, byte[] IV){	 
-		byte[] next;
-		byte[] outputText;
-		byte[] bceText;
-		
-		bceText = algo.cryptBlock
-	}*/
 }
